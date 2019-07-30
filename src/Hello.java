23e4bb06cbf04dde54777ccfeee606fbd62e6bfc
josanesouza <josanesouza7@gmail.com>
2019-07-30 17:07:35 -0300

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,10 @@
 
 public class Hello {
 
+	public static int  soma(int a , int b){
+	 return a+b;
+	}
+	
 	public static void main(String[] args) {
 		// Prints "Hello, World" to the terminal window.
 		System.out.println("Hello, World");
